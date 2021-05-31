f51795f068fba34ca2f85efd58bc3e16cd9a5c97
AllanKirui <nallawilljr@gmail.com>
2021-06-01 00:26:06 +0300

    Add mobile menu, Add gradient background to mobile menu, Add fade in fade out animation to mobile menu and gradient background

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,25 @@
 // Get the header
+const burger_el = document.getElementById("burger");
+const body_el = document.querySelector("body");
 const header_el = document.getElementById("header");
+const menu = document.getElementById("menu");
+const faded_bg = document.getElementById("faded");
+const fading_in_el = document.querySelectorAll(".to-fade");
 
-header_el.addEventListener("click", () => {
+burger.addEventListener("click", () => {
+   header_el.classList.toggle("open");
    if (header_el.classList.contains("open")) {
-      header_el.classList.remove("open");
+      body_el.classList.add("no-scroll");
+      fading_in_el.forEach((el) => {
+         el.classList.remove("fades-out");
+         el.classList.add("fades-in");
+      });
+      faded_bg.classList.add("faded-bg");
    } else {
-      header_el.classList.add("open");
+      fading_in_el.forEach((el) => {
+         el.classList.remove("fades-in");
+         el.classList.add("fades-out");
+      });
+      body_el.classList.remove("no-scroll");
    }
 });
